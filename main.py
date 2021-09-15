3e054aca8347da80c3d8e7f336dba72bb5e69413
ravelraise <ravelraise@gmail.com>
2021-09-16 01:57:41 +0300

    1) lambda-functions turn to def 2) some changes with view

--- main.py
+++ main.py
@@ -31,16 +31,16 @@ def Output(isHasDelays, ifOrderCalc):
         axs[0].grid()
         axs[0].set_title("NUMERICAL SOLUTION")
         axs[0].legend(loc='upper left', fontsize=12)
-        axs[0].set_xlabel("TIME", fontsize=14);
+        axs[0].set_xlabel("TIME", fontsize=14)
         axs[0].set_ylabel("SOLUTION", fontsize=14)
 
-        nb = 1;
-        n = 8
-        err = [];
+        nb = 1
+        n = 7
+        err = []
         nsteps = []
 
         for steppow in range(nb, n):
-            stepsize = pow(2, -steppow)
+            stepsize = 2**(-steppow)
             if isHasDelays:
                 sol = ide_delay_solve(idefun, delays, K, delays_int, history, tspan, stepsize)
             else:
@@ -50,13 +50,13 @@ def Output(isHasDelays, ifOrderCalc):
 
         print("Convergence order:", (log10(err[-1]) - log10(err[-2])) / (log10(pow(2, -n)) - log10(pow(2, -n + 1))))
 
-        axs[1].plot(nsteps, err);
-        axs[1].grid();
+        axs[1].plot(nsteps, err)
+        axs[1].grid()
 
-        axs[1].set_title("CONVERGENCE ORDER");
-        axs[1].set_xscale('log');
+        axs[1].set_title("CONVERGENCE ORDER")
+        axs[1].set_xscale('log')
         axs[1].set_yscale('log')
-        axs[1].set_xlabel("STEPSIZE", fontsize=14);
+        axs[1].set_xlabel("STEPSIZE", fontsize=14)
         axs[1].set_ylabel("ERROR", fontsize=14)
 
         fig.tight_layout()
@@ -67,20 +67,21 @@ def Output(isHasDelays, ifOrderCalc):
         else:
             for i in range(nz):
                 fig = plt.plot(sol[0], sol[1][i], label="y" + str(i + 1) + "(t)")
-        plt.title("NUMERICAL SOLUTION");
+        plt.title("NUMERICAL SOLUTION")
         plt.legend(fontsize=14, bbox_to_anchor=(1, 1))
-        plt.xlabel("TIME", fontsize=14);
-        plt.ylabel("SOLUTION", fontsize=14);
+        plt.xlabel("TIME", fontsize=14)
+        plt.ylabel("SOLUTION", fontsize=14)
         plt.grid()
+
 
 # Example 1 (only integral)
 tspan = [1.1, 5]
-idefun = lambda t, y, i: ((t - 1) * exp(t ** 2) * i) / (exp(-1) * y - 1)
-K = lambda t, s, y: [y * exp(-s * t)]
-delays_int = lambda t: [t - 1]  # delays of integrals
-history = lambda t: exp(t)
+def idefun(t, y, i): return ((t - 1) * exp(t ** 2) * i) / (exp(-1) * y - 1)
+def      K(t, s, y): return [y * exp(-s * t)]
+def   delays_int(t): return [t - 1]  # delays of integrals
+def      history(t): return exp(t)
 
-fun = lambda t: exp(t)
+def fun(t): return exp(t)
 analytic_sol = fun(tspan[1])
 
 sol = ide_solve(idefun, K, delays_int, history, tspan, 1e-2)
@@ -88,15 +89,14 @@ Output(0, 0)
 Output(0, 1)
 
 # Example 2 (integral+discrete delays)
+tspan = [0, 10]
+def idefun(t, y, z, i): return (1 + exp(-pi / 2)) * y - exp(-pi / 2) * z - 2 * exp(-2 * t) * i
+def         K(t, s, y): return [y * exp(t + s)]
+def       delays(t, y): return [t - pi / 2]  # delays of z
+def      delays_int(t): return [t - pi / 2]
+def         history(t): return cos(t)
 
-tspan      = [0, 10]
-idefun     = lambda t,y,z,i: (1+exp(-pi/2))*y - exp(-pi/2)*z - 2*exp(-2*t)*i
-K          = lambda t,s,y:   [y*exp(t+s)]
-delays     = lambda t,y:     [t-pi/2] # delays of z
-delays_int = lambda t:       [t-pi/2]
-history    = lambda t:       cos(t)
-
-fun = lambda t: cos(t)
+def fun(t): return cos(t)
 analytic_sol = fun(tspan[1])
 
 # sol = ide_delay_solve(idefun, delays, K, delays_int, history, tspan, 1e-2)
