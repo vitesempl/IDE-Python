69cf595c0d1d7193c7e468158ebe8c3819955c1f
ravelraise <ravelraise@gmail.com>
2021-09-17 18:12:37 +0300

    edit setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ from setuptools import setup, find_packages
 setup(
     name="RK",
 
+    version='1.0',
+
     author="Aleksandr Lobaskin, Alexey Eremin, SPbU",
 
     packages=find_packages(exclude=['RK']),
